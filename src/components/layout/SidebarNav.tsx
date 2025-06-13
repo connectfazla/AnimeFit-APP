19ebe35ed8b67a2a79fd42ab71877046f46fe624
Fazla Rabbi <fazla@uppearance.com>
2025-06-13 11:44:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/SidebarNav.tsx
+++ src/components/layout/SidebarNav.tsx
@@ -31,7 +31,7 @@ export function SidebarNav() {
         <SidebarMenu>
           {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} legacyBehavior passHref>
+              <Link href={item.href}>
                 <SidebarMenuButton
                   variant="default"
                   size="default"
